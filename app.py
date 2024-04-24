3f4a4d68c8f1dc63bca23663b9fec28557f2f582
Fernando Mocrosky <fernandobismaia@gmail.com>
2024-04-24 16:47:28 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+app = Flask(__name__)
+
+@app.route('/scrap')
+def index():
+  options = Options()
+  options.headless = True
+
+  service = Service("./chromedriver.exe")
+  driver = webdriver.Chrome(service=service, options=options)
+
+  try:
+    driver.get("http://www.bianca.com")
+    element = driver.find_element(By.TAG_NAME, "h1")
+    text = element.text
+  finally:
+    driver.quit()
+
+  data = {
+    "scrapped": text
+  }
+  return jsonify(data)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
